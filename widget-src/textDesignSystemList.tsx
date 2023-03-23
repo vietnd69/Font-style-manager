4e70fbdffea4a805331399b1f2c7311056b7f9da
vietnd <vboy234@gmail.com>
2023-03-23 17:40:11 +0700

    update choice font

---
+++ widget-src/textDesignSystemList.tsx
@@ -0,0 +1,11 @@
+const { widget } = figma;
+const { AutoLayout, Text, useSyncedState, Input, SVG } = widget;
+
+const TextDesignList = () => {
+
+    return <AutoLayout>
+        <Text>TextDesignList</Text>
+    </AutoLayout>
+}
+
+export default TextDesignList
---
+++ widget-src/textStyleManager.tsx
@@ -0,0 +1,11 @@
+const { widget } = figma;
+const { AutoLayout, Text, useSyncedState, Input, SVG } = widget;
+
+const TextStyleManager = () => {
+
+    return <AutoLayout>
+        <Text>TextStyleManager</Text>
+    </AutoLayout>
+}
+
+export default TextStyleManager
